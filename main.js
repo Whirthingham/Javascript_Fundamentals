847c6baa3151e6a4bd221c727013706ad5c165e6
Whirthman <whirthman@gmail.com>
2023-06-16 15:54:09 +0000

    How to access elements by loop

--- main.js
+++ main.js
@@ -8,3 +8,13 @@ console.log(salad[salad.length-2]);
 console.log(salad[salad.length]);
 console.log(salad[-1]);
 console.log(salad[salad.length+1])
+
+// How to access elements by loop
+for(let i=0; i<=salad.length-1; i++){
+    console.log(salad[i]);
+}
+
+//Access element by using forEach loop
+salad.forEach((ingredients)=>{
+    console.log(ingredients);
+})
