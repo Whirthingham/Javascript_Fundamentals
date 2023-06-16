49e0a442dbd296a0bc9d360736ba6188bbc1c37c
Whirthman <whirthman@gmail.com>
2023-06-16 15:48:42 +0000

    How to get Elements from an Array

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 // How to Create an Array in JavaScript
 const salad = ['Cabbage','Cucumber','Carrot','Onion','Tomatoes','Green Pepper', 'Eggs', 'Sardine', 'Ketchup'];
 console.log(salad);
+//How to Get Elements from an Array in JS
+console.log(salad[0])
+console.log(salad[salad.length-1])
+console.log(salad[salad.length-2]);
+console.log(salad[salad.length]);
+console.log(salad[-1]);
+console.log(salad[salad.length+1])
